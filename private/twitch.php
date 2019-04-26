734ed47f0a0d8d7df2922fcc00434a2c54f667d0
Mike Kasprzak <mike@sykhronics.com>
2019-04-26 12:48:34 -0400

    figured out how Twitch's API works

--- private/twitch.php
+++ private/twitch.php
@@ -1,2 +1,34 @@
 <?php
 
+namespace twitch;
+
+const USER_IDS = [
+	"69760921", // LudumDare
+];
+
+const GAME_IDS = [
+	"509670",	// Science & Technology
+	"509660",	// Art
+	"509673",	// Makers & Crafting
+	"509663",	// Special Events
+	"26936",	// Music & Performing Arts
+	"509667",	// Food & Drink
+];
+
+// Obsolete
+const COMMUNITY_IDS = [
+	"a7b7373b-2c36-43a6-977b-9f9290154213",	// Creative
+];
+
+// Lookup Tags: https://www.twitch.tv/directory/all/tags/c8d6d6ee-3b02-4ae7-81e9-4c0f434941cc
+const TAG_IDS = [
+	"c8d6d6ee-3b02-4ae7-81e9-4c0f434941cc",	// Ludum Dare
+	"f588bd74-e496-4d11-9169-3597f38a5d25",	// Game Development
+	"a59f1e4e-257b-4bd0-90c7-189c3efbf917",	// Programming
+	"c23ce252-cf78-4b98-8c11-8769801aaf3a",	// Web Development
+
+	"6ea6bca4-4712-4ab9-a906-e3336a9d8039",	// English
+];
+
+const
+
