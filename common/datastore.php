a6c3f926b68911d89b74887d0dfa1ae75ed0413c
Mike Kasprzak <mike@sykhronics.com>
2019-05-01 00:27:30 -0400

    too tired to finish

--- common/datastore.php
+++ common/datastore.php
@@ -3,14 +3,23 @@
 
 $_redis = new Redis();
 $_redis->Connect(REDIS_HOST, REDIS_PORT);
-#$_redis->setOption(Redis::OPT_PREFIX, REDIS_PREFIX);	// Optionally prefix all keys (might break interoperability)
+#$_redis->setOption(Redis::OPT_PREFIX, REDIS_PREFIX);					// Optionally prefix all keys (might break interoperability)
+#$_redis->setOption(Redis::OPT_SERIALIZER, Redis::SERIALIZER_PHP);		// Use built-in serialize/unserialize
+#$_redis->setOption(Redis::OPT_SERIALIZER, Redis::SERIALIZER_IGBINARY);	// Use igBinary serialize/unserialize
+#$_redis->setOption(Redis::OPT_SERIALIZER, Redis::SERIALIZER_MSGPACK);	// Use msgpack serialize/unserialize
 
 function ds_Set($key, $value, $ttl = null) {
 	global $_redis;
-	return $_redis->set($key, $value, $ttl);
+	return $_redis->set($key, serialize($value), $ttl);
 }
 
 function ds_Get($key) {
 	global $_redis;
-	return $_redis->get($key);
+	$ret = $_redis->get($key);
+	return $ret ? unserialize($ret) : null;
+}
+
+function ds_Unlink($key) {
+	global $_redis;
+	return $_redis->unlink($key);
 }
--- common/fetch_twitch.php
+++ common/fetch_twitch.php
@@ -10,8 +10,74 @@ const API_BASE = "https://api.twitch.tv/helix/";
 const ID_BASE = "https://id.twitch.tv/oauth2/";
 
 // Lookup the bearer, if we have it
-const BEARER_API_KEY = "TWITCH_BEARER_API_KEY";
-$bearer = ds_Get(BEARER_API_KEY);
+const BEARER_API_BASE = "TWITCH_BEARER_API_";
+$bearer = ds_Get(BEARER_API_BASE."KEY");
+$bearer_modified = ds_Get(BEARER_API_BASE."MODIFIED");
+// todo: that false check thing
+
+echo "b: ".json_encode($bearer)."    bm: ".$bearer_modified."\n";
+
+function fetch_TwitchID($url, $postdata = null, $headers = []) {
+	echo $url."/n";
+
+	return Fetch_Json(ID_BASE.$url, $postdata, $headers);
+}
+
+function token_GetClientCredentials() {
+	$qs = "";
+	QueryString_Add($qs, "client_id", TWITCH_CLIENT_ID);
+	QueryString_Add($qs, "client_secret", TWITCH_CLIENT_SECRET);
+	QueryString_Add($qs, "grant_type", "client_credentials");
+
+	return fetch_TwitchID("token?".$qs, "");
+}
+
+function token_Validate($token = null) {
+	global $bearer;
+	if (is_null($token)) {
+		$token = $bearer;
+	}
+
+	if (!$token) {
+		$ret = fetch_TwitchID("validate", "" /* trigger HTTP POST */, ["Authorization: OAuth ".$token]);
+		return $ret && array_key_exists('client_id', $ret) && ($ret['client_id'] == TWITCH_CLIENT_ID);
+	}
+	return null;
+}
+
+function token_Revoke($token = null) {
+	global $bearer;
+	if (is_null($token)) {
+		$token = $bearer;
+	}
+
+	if ($token) {
+		$qs = "";
+		QueryString_Add($qs, "client_id", TWITCH_CLIENT_ID);
+		QueryString_Add($qs, "token", $token);
+
+		return fetch_TwitchID("revoke?".$qs, "" /* trigger HTTP POST */);
+	}
+	return null;
+}
+
+function token_Do() {
+	global $bearer;
+	global $bearer_modified;
+
+	if (!$bearer) {
+		$bearer = token_GetClientCredentials();
+		$bearer_modified = time();
+
+		echo "b: ".json_encode($bearer)."    bm: ".$bearer_modified."\n";
+
+		ds_Set(BEARER_API_BASE."KEY", $bearer, 10);
+		ds_Set(BEARER_API_BASE."MODIFIED", $bearer_modified, 10);
+	}
+}
+token_Do();
+
+
 
 function fetch_TwitchAPI($url, $postdata = null, $_headers = null) {
 	global $bearer;
@@ -33,23 +99,3 @@ function fetch_TwitchAPI($url, $postdata = null, $_headers = null) {
 	// Fetch
 	return Fetch_Json(API_URL.$url, $postdata, $headers);
 }
-
-
-function fetch_TwitchID($url, $postdata = null, $_headers = null) {
-	// Build headers list
-	$headers = [];
-//	//	"Accept: application/vnd.twitchtv.v5+json",
-//		"Client-ID: ".TWITCH_CLIENT_ID
-//	];
-//	//$headers[] = "Authorization: Bearer ".$token;
-
-	if (!is_null($_headers)) {
-		$headers = array_merge($headers, $_headers);
-	}
-
-	// Fetch
-	return Fetch_Json(ID_URL.$url, $postdata, $headers);
-}
-
-// fetch twitch bearer oath
-
